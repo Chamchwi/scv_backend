3d7e9bd3011cf370a5ed0c3719d09efd33537254
Smaker <admin@smaker.kr>
2016-12-27 16:05:16 +0900

    Clean import & Code

--- src/main/java/kr/smaker/scv/Controller/HomeController.java
+++ src/main/java/kr/smaker/scv/Controller/HomeController.java
@@ -1,25 +1,11 @@
 package kr.smaker.scv.Controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
-import org.codehaus.jackson.map.util.JSONPObject;
-import org.json.simple.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.smaker.scv.manager.DBService;
 import kr.smaker.scv.manager.UTF8Response;
@@ -29,8 +15,7 @@ import kr.smaker.scv.manager.UTF8Response;
  */
 @Controller
 public class HomeController {
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	@Autowired
 	private DBService db;
 	
--- src/main/java/kr/smaker/scv/Controller/UserController.java
+++ src/main/java/kr/smaker/scv/Controller/UserController.java
@@ -1,29 +1,17 @@
 package kr.smaker.scv.Controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
-
-import kr.smaker.scv.dao.UserData;
 import kr.smaker.scv.manager.DBService;
-import kr.smaker.scv.manager.RemoveBOM;
 import kr.smaker.scv.manager.UTF8Response;
 
 @Controller
@@ -45,8 +33,7 @@ public class UserController {
 		map.put("password", password);
 		map.put("nickname", nickname);
 
-		if ((email == null) || (password == null)
-				|| (nickname == null)) {
+		if ((email == null) || (password == null) || (nickname == null)) {
 			return new UTF8Response("{\"success\":false, \"errorcode\":\"오류 발생! 다시 입력해주세요.\"}", "json").entity;
 		}
 
@@ -56,7 +43,7 @@ public class UserController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return new UTF8Response("{\"success\":false}", "json").entity;
 	}
 
@@ -70,11 +57,11 @@ public class UserController {
 		map.put("email", email);
 		map.put("token", token);
 		map.put("nickname", nickname);
-		
+
 		if ((email == null) || (nickname == null)) {
 			return new UTF8Response("{\"success\":false, \"errorcode\":\"오류 발생! 다시 입력해주세요.\"}", "json").entity;
 		}
-		
+
 		try {
 			db.fb_register(map);
 			return new UTF8Response("{\"success\":true}", "json").entity;
