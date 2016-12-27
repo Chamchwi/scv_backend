fd4a2f97854e5dc74b95ce0a078f41b290cd5f7f
Smaker <admin@smaker.kr>
2016-12-27 22:54:56 +0900

    Clean 'RoomController.class'

--- src/main/java/kr/smaker/scv/Controller/RoomController.java
+++ src/main/java/kr/smaker/scv/Controller/RoomController.java
@@ -16,38 +16,13 @@ import kr.smaker.scv.manager.DBService;
 import kr.smaker.scv.manager.UTF8Response;
 
 
-@Controller()
+@Controller
 
-@RequestMapping(value = "/API", method = RequestMethod.GET)
+@RequestMapping(value = "/API")
 public class RoomController {
 	
 	@Autowired
 	private DBService db;
-	
-	@RequestMapping(value = "/Room/list", method = RequestMethod.GET)
-	public ResponseEntity<String> register(@RequestParam("id") String id) {
-		System.out.println("ID : " + id);
-		
-		return new UTF8Response("{\"success\":false, \"errorcode\":\"403043\"}", "json").entity;
-
-	}
-	
-	@RequestMapping(value = "/Room/wait", method = RequestMethod.POST)
-	public ResponseEntity<String> Waitroom(@RequestParam("email")  String email) {
-		HashMap <String, Object> map = new HashMap <String, Object> ();
-		
-		
-		return new UTF8Response("{\"success\":false, \"errorcode\":\"403043\"}", "json").entity;
-	}
-	
-	@RequestMapping(value = "/Room/create", method = RequestMethod.POST)
-	public ResponseEntity<String> createRoom(@RequestParam("email")  String email
-			,@RequestParam("gameMode") String gameMode) {
-		HashMap <String, Object> map = new HashMap <String, Object> ();
-		
-		
-		return new UTF8Response("{\"success\":false, \"errorcode\":\"403043\"}", "json").entity;
-	}
 	
 	 
 }
