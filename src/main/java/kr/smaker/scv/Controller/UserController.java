e599c0f855ab44dd6605cb7f9b942a3ee12b1258
Smaker <admin@smaker.kr>
2016-12-27 12:27:32 +0900

    Register API Service v1

--- src/main/java/kr/smaker/scv/Controller/UserController.java
+++ src/main/java/kr/smaker/scv/Controller/UserController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
+
 import kr.smaker.scv.dao.UserData;
 import kr.smaker.scv.manager.DBService;
 import kr.smaker.scv.manager.RemoveBOM;
@@ -30,7 +32,7 @@ public class UserController {
 
 	@Autowired
 	private DBService db;
-	
+
 	@RequestMapping(value = "/normal_register", method = RequestMethod.POST)
 	public ResponseEntity<String> normal_register(HttpServletRequest request) {
 		String email = request.getParameter("email");
@@ -42,74 +44,41 @@ public class UserController {
 		map.put("password", password);
 		map.put("nickname", nickname);
 
+		if ((email == null) || (password == null)
+				|| (nickname == null)) {
+			return new UTF8Response("{\"success\":false, \"errorcode\":\"오류 발생! 다시 입력해주세요.\"}", "json").entity;
+		}
+
 		try {
 			db.normal_register(map);
-			return new UTF8Response("{\"success\":true\"}", "json").entity;
+			return new UTF8Response("{\"success\":true}", "json").entity;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	@RequestMapping(value ="/fb_register", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/fb_register", method = RequestMethod.POST)
 	public ResponseEntity<String> facebook_register(HttpServletRequest request) {
 		String email = request.getParameter("email");
 		String token = request.getParameter("token");
 		String nickname = request.getParameter("nickname");
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		
+
 		map.put("email", email);
 		map.put("token", token);
 		map.put("nickname", nickname);
 		
+		if ((email == null) || (nickname == null)) {
+			return new UTF8Response("{\"success\":false, \"errorcode\":\"오류 발생! 다시 입력해주세요.\"}", "json").entity;
+		}
+		
 		try {
 			db.fb_register(map);
-			return new UTF8Response("{\"success\":true\"}", "json").entity;
+			return new UTF8Response("{\"success\":true}", "json").entity;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		return null;
 	}
-
-	/* @RequestMapping(value = "/fb_login", method = RequestMethod.GET)
-	public ResponseEntity<String> facebookLogin(HttpServletRequest request) throws Exception {
-
-		System.out.println("NAME : " + request.getParameter("first_name"));
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		UserData data = new UserData();
-
-		if (request.getParameter("email") == null) {
-			return new UTF8Response("{\"success\":false\", \"errorcode\":\"이메일을 입력해주세요.\"}", "json").entity;
-		} else if (request.getParameter("first_name") == null) {
-			return new UTF8Response("{\"success\":false\", \"errorcode\":\"first_name을 입력해주세요.\"}", "json").entity;
-		} else if (request.getParameter("last_name") == null) {
-			return new UTF8Response("{\"success\":false\", \"errorcode\":\"last_name을 입	력해주세요.\"}", "json").entity;
-		} else if ((request.getParameter("email") == null) && (request.getParameter("first_name") == null)
-				&& (request.getParameter("last_name") == null)) {
-			return new UTF8Response("{\"success\":false\", \"errorcode\":\"입력된 값이 없습니다.\"}", "json").entity;
-
-		}
-
-		data.setEmail(request.getParameter("email"));
-		String email = data.getEmail();
-		if (email.equals(request.getParameter("email"))) {
-			map.put("email", request.getParameter("email"));
-			map.put("first_name", request.getParameter("first_name"));
-			map.put("last_name", request.getParameter("last_name"));
-			map.put("last_log", null);
-			map.put("score", 1000);
-			map.put("winning_streak", 0);
-
-			try {
-				// db.register(map);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return new UTF8Response("{\"success\":true}", "json").entity;
-		} else {
-			return new UTF8Response("{\"success\":false}", "json").entity;
-		}
-
-	} */
 }
